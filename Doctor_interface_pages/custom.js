76db810915469b14dbf8abc8c6e3c75ea909d697
Himank <himank@Himanks-MacBook-Pro.local>
2019-04-11 13:41:57 +0530

    inital

---
+++ Doctor_interface_pages/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+	{
+	$("#btclick").click(function(){
+	var url= 'G:/proj/give_medi.html';
+	$(location).attr('href',url);
+	})
+	});
---
+++ health_home_page/helth/js/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 10/17/2015.
+ */
+
